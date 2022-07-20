568b76c0b741ccec29bbc7f2e6fc6f303bf9143b
Hssan Driss <drissh@informatik.uni-freiburg.de>
2022-07-20 14:41:43 +0200

    belif matching implemented

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -8,11 +8,12 @@ from .lovasz_loss import LovaszLoss
 from .tversky_loss import TverskyLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .dummy_loss import DummyLoss, dummy_loss
+from .belief_matching_loss import BeliefMatchingLoss
 from .ldam_loss import LDAMLoss
 from .edl_loss import EDLLoss
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
     'weight_reduce_loss', 'weighted_loss', 'LovaszLoss', 'DiceLoss',
-    'FocalLoss', 'TverskyLoss', 'LDAMLoss', 'EDLLoss'
+    'FocalLoss', 'TverskyLoss', 'LDAMLoss', 'EDLLoss', 'BeliefMatchingLoss'
 ]
