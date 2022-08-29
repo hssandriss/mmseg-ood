27027f54777bc25d73357102408285c0237330c2
Hssan Driss <drissh@informatik.uni-freiburg.de>
2022-08-29 10:34:38 +0200

    testing first with ce

--- configs/deeplabv3/deeplabv3_r50-d8_720x720_70e_cityscapes_nf_bll.py
+++ configs/deeplabv3/deeplabv3_r50-d8_720x720_70e_cityscapes_nf_bll.py
@@ -4,9 +4,9 @@ _base_ = [
     '../_base_/schedules/schedule_70e.py'
 ]
 model = dict(
-    # decode_head=dict(type='ASPPNfBllHead', align_corners=True, num_classes=19,), NLL(CE)
-    decode_head=dict(type='ASPPNfBllHead', align_corners=True, num_classes=19, nsamples_train=2,
-                     loss_decode=dict(type='EDLLoss', num_classes=19, loss_variant='mll')),
+    decode_head=dict(type='ASPPNfBllHead', align_corners=True, num_classes=19,),  # NLL(CE)
+    # decode_head=dict(type='ASPPNfBllHead', align_corners=True, num_classes=19, nsamples_train=2,
+    #                  loss_decode=dict(type='EDLLoss', num_classes=19, loss_variant='mll')),
 
 
     # auxiliary_head=dict(align_corners=True),
