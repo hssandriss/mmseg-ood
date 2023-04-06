f0d95a438c87e74f39f4d68f99cdad5de13f4ca2
Hssan Driss <drissh@informatik.uni-freiburg.de>
2023-04-06 23:51:57 +0200

    minor fix: missing import after pre-commit

--- cityscape_classes.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-with open("class_count_cityscapes_pixel.npy", "rb") as f:
-    data = np.load(f)
-data = data[:-1]
-classes = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole', 'traffic light', 'traffic sign', 'vegetation',
-           'terrain', 'sky', 'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle', 'bicycle')
-print([(classes[i], data[i],i) for i in np.argsort(data)])
