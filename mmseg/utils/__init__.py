0b0e55ab6efa6e7d358cc5b1a40ec842ebd6ed83
Hssan Driss <drissh@informatik.uni-freiburg.de>
2023-04-06 23:39:41 +0200

    minor fix: missing import after pre-commit

--- mmseg/utils/__init__.py
+++ mmseg/utils/__init__.py
@@ -4,11 +4,13 @@ from .logger import get_root_logger
 from .misc import find_latest_checkpoint
 from .ood_metrics import brierscore, diss
 from .ood_metrics import get_measures as get_ood_measures
+from .ood_metrics import print_measures, print_measures_with_std
 from .set_env import setup_multi_processes
 from .util_distribution import build_ddp, build_dp, get_device
 
 __all__ = [
     'get_root_logger', 'collect_env', 'find_latest_checkpoint',
     'setup_multi_processes', 'get_ood_measures', 'build_ddp', 'build_dp',
-    'get_device', 'brierscore', 'diss'
+    'get_device', 'brierscore', 'diss', 'print_measures',
+    'print_measures_with_std'
 ]
