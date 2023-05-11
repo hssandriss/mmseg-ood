f0445cbd2afd8428e5fc8614c5e824a4d6bc417e
Hssan Driss <drissh@informatik.uni-freiburg.de>
2023-05-11 18:34:31 +0200

    minor fix

--- configs/segmenter/segmenter_vit-s_linear_512x512_120e_streethazards_edl_subtest.py
+++ configs/segmenter/segmenter_vit-s_linear_512x512_120e_streethazards_edl_subtest.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/segmenter_vit-b16_mask.py', '../_base_/datasets/street_hazards_512x512.py',
+    '../_base_/models/segmenter_vit-b16_mask.py', '../_base_/datasets/street_hazards_512x512_subtest.py',
     '../_base_/epoch_runtime.py', '../_base_/schedules/schedule_120e.py'
 ]
 
